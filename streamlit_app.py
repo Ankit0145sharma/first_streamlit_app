0076375a1a80c77221b14e96a51761ccc8de5f55
Ankit0145sharma <123903480+Ankit0145sharma@users.noreply.github.com>
2023-02-01 12:38:53 +0530

    Update streamlit_app.py
    adding more data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('kale,spinach and Rocket Smoothie')
+streamlt.text('Hard-Boiled Free_Ranges Egg')
